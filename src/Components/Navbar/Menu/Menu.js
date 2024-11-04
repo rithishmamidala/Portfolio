596dd7cd05b6e0b0382c16ddc40e0eeb64be6270
rithishmamidala <rithishrockzz@gmail.com>
2024-11-04 23:54:11 +0530

    navbar

---
+++ src/Components/Navbar/Menu/Menu.js
@@ -0,0 +1,20 @@
+
+import './Menu.css';
+const Menu = () => {
+    return(
+        
+    <nav class="menu menu-1">
+  <ul>
+    <li><a href="#">About Me</a></li>
+    <li><a href="#">career</a></li>
+    <li><a href="#">Projects</a></li>
+    <li><a href="#">Certificates</a></li>
+    
+  </ul>
+</nav>
+        
+        
+    )
+}
+
+export default Menu;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+
+import Menu from './Menu/Menu';
+import './Navbar.css'
+const Navbar = ()  => {
+    return (
+
+
+        <div className ="Nav"> 
+
+        <div className = "Name"> <a href="#">  𝓡𝓲𝓽𝓲𝓼𝓱 𝓜𝓪𝓶𝓲𝓭𝓪𝓵𝓪   </a></div>
+        <div className="Menu">  <Menu/></div>
+        </div>
+    )
+}
+
+export default Navbar;
