9c829574989c4f58164fa528c5932b8f77f51cad
rithishmamidala <rithishrockzz@gmail.com>
2024-11-05 12:10:38 +0530

    Home1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
 import './App.css';
 
+import Home from './Components/Home/Home';
+
 import Navbar  from './Components/Navbar/Navbar';
 function App() {
   return (
     <> 
 
     <Navbar/>
+    <Home/>
+   
+  
       
     
     </>
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,35 @@
+
+import './Home.css';
+const Home = () => {
+    return (
+        <> 
+        <div className="Home"> 
+            <div className="Info">
+                <h1>Hello, </h1>
+                <h1>I'm Rithish Mamidala</h1>
+                <p>a passionate full-stack developer with experience in UI/UX design. I specialize in building elegant and functional web applications, focusing on both user experience and performance </p>
+                <div className="DP"> </div>   
+              
+                
+             </div>
+             <div className="Buttons">
+             
+
+<button class="DownloadButton">
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"></path>
+  </svg>
+  <div class="text">
+    Button
+  </div>
+</button>
+                 <button>Contact Me</button>
+                  </div> 
+             
+         </div>
+        
+        </>
+
+    )
+}
+export default Home;
